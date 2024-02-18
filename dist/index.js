96a11f4f27db816c5ad5f3d264cf793a76ce3b90
Joar Heimonen <joarheimonen@live.no>
2024-02-18 23:40:21 +0100

    Added dist to gitignore

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const gitStats_1 = require("./gitStats");
-(0, gitStats_1.fetchUserData)("slenderman00", (data) => {
-    console.log(data);
-});
