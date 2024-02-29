3578664d74c6cfcb22f3f44faece360d4ff0100e
Joar Heimonen <joarheimonen@live.no>
2024-02-29 20:33:13 +0100

    Added language percentages

--- src/gitStats.ts
+++ src/gitStats.ts
@@ -1,56 +1,82 @@
 import { graphql } from "@octokit/graphql";
 import dotenv from "dotenv";
-dotenv.config({ path: 'SECRETS.env' })
+dotenv.config({ path: "SECRETS.env" });
 
 const graphqlWithAuth = graphql.defaults({
-    headers: {
-        authorization: `bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`
-    }
-})
+  headers: {
+    authorization: `bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+  },
+});
 
-export async function fetchUserData(username: string, callback: (userData: object) => void) {
-    const now = new Date().toISOString();
-    let lastYearMS = Date.now() - 31556952000;
-    const lastYear = new Date(lastYearMS).toISOString();
+export async function fetchUserData(
+  username: string,
+  callback: (userData: object) => void
+) {
+  const now = new Date().toISOString();
+  let lastYearMS = Date.now() - 31556952000;
+  const lastYear = new Date(lastYearMS).toISOString();
 
-    try {
-        let data: object = await graphqlWithAuth({
-            "query": `query {
+  try {
+    let data: object = await graphqlWithAuth({
+      query: `query {
                 user(login: "${username}") {
-                repositories(last: 100) {
-                    totalCount
-                    nodes {
-                    name
-                    stargazerCount
-                    visibility
-                    }
-                }
-                createdAt
-                contributionsCollection(
-                    from: "${lastYear}"
-                    to: "${now}"
-                ) {
-                    contributionCalendar {
-                    totalContributions
-                    weeks {
-                        contributionDays {
-                        weekday
-                        date
-                        contributionCount
-                        color
+                    repositories(last: 100) {
+                        totalCount
+                        nodes {
+                        name
+                        stargazerCount
+                        visibility
+                        languages(first: 100) {
+                            nodes {
+                              name
+                            }
+                          }
                         }
-                        firstDay
                     }
+                    createdAt
+                    contributionsCollection(
+                        from: "${lastYear}"
+                        to: "${now}"
+                    ) {
+                        contributionCalendar {
+                        totalContributions
+                        weeks {
+                            contributionDays {
+                            weekday
+                            date
+                            contributionCount
+                            color
+                            }
+                            firstDay
+                        }
+                        }
                     }
                 }
-                }
-            }`
-        })
+            }`,
+    });
 
-        callback(data)
-    } catch(err) {
-        console.error(err)
-        return;
-    }
+    callback(data);
+  } catch (err) {
+    console.error(err);
+    return;
+  }
+}
 
+export function getLanguages(data: any) {
+  let _languages: any = {};
+  let total = 0;
+  let repos = data.user.repositories.nodes;
+  repos.forEach((repo: any) => {
+    let languages = repo.languages.nodes;
+    languages.forEach((language: any) => {
+      if (_languages.hasOwnProperty(language.name)) {
+        _languages[language.name] += 1;
+      } else {
+        _languages[language.name] = 1;
+      }
+      total++;
+    });
+  });
+
+  return { total: total, languages: _languages };
 }
--- src/imageGen.ts
+++ src/imageGen.ts
@@ -1,95 +1,152 @@
 import { createCanvas } from "canvas";
+import { getLanguages } from "./gitStats";
 
-function roundRect(ctx: any, x: number, y: number, width: number, height: number, radius: number) {
-    ctx.beginPath();
-    ctx.moveTo(x + radius, y);
-    ctx.arcTo(x + width, y, x + width, y + height, radius);
-    ctx.arcTo(x + width, y + height, x, y + height, radius);
-    ctx.arcTo(x, y + height, x, y, radius);
-    ctx.arcTo(x, y, x + width, y, radius);
-    ctx.closePath();
+function roundRect(
+  ctx: any,
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  radius: number
+) {
+  ctx.beginPath();
+  ctx.moveTo(x + radius, y);
+  ctx.arcTo(x + width, y, x + width, y + height, radius);
+  ctx.arcTo(x + width, y + height, x, y + height, radius);
+  ctx.arcTo(x, y + height, x, y, radius);
+  ctx.arcTo(x, y, x + width, y, radius);
+  ctx.closePath();
 
-    ctx.fill();
+  ctx.fill();
 }
 
 function compareStars(a: any, b: any) {
-    return b.stargazerCount - a.stargazerCount;
+  return b.stargazerCount - a.stargazerCount;
 }
 
-function generateGraph(ctx: any, data: any, width: number, height: number, lineWidth: number) {
-    const length = data.length
-    const steps = width / length;
+function comparePercentage(a: any, b: any) {
+    return b.percentage - a.percentage;
+}
 
-    ctx.lineWidth = lineWidth;
-    ctx.beginPath();
+function generateGraph(
+  ctx: any,
+  data: any,
+  width: number,
+  height: number,
+  lineWidth: number
+) {
+  const length = data.length;
+  const steps = width / length;
 
-    data.forEach((day: any, index: number) => {
-        const x = index * steps;
-        const y = (height - day.contributionCount * 2) - 2;
+  ctx.lineWidth = lineWidth;
+  ctx.beginPath();
 
-        if(day.contributionCount == 0) {
-            ctx.lineTo(x, y);
-            return;
-        }
+  data.forEach((day: any, index: number) => {
+    const x = index * steps;
+    const y = height - day.contributionCount * 2 - 2;
 
-        if (index === 0) {
-            ctx.moveTo(x, y);
-        } else {
-            const cp1x = x - steps / 2;
-            const cp1y = Math.max(y * 0.9, 0);
+    if (day.contributionCount == 0) {
+      ctx.lineTo(x, y);
+      return;
+    }
 
-            const cp2x = x - steps / 2;
-            const cp2y = Math.max(y * 0.9, 0);
+    if (index === 0) {
+      ctx.moveTo(x, y);
+    } else {
+      const cp1x = x - steps / 2;
+      const cp1y = Math.max(y * 0.9, 0);
 
-            ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
-        }
-    });
+      const cp2x = x - steps / 2;
+      const cp2y = Math.max(y * 0.9, 0);
 
-    ctx.stroke(); 
+      ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
+    }
+  });
+
+  ctx.stroke();
 }
 
 function generateTag(ctx: any, x: number, y: number, data: string): void {
-    const fontSize = 15;
-    const padding = 10;
-    
-    ctx.font = `${fontSize}px Arial`;
-    
-    const textWidth = ctx.measureText(data).width;
-    const tagWidth = textWidth + 2 * padding;
-    const tagHeight = fontSize + 6;
+  const fontSize = 15;
+  const padding = 10;
 
-    roundRect(ctx, x - padding, y - fontSize + 2, tagWidth, tagHeight, 3);
+  ctx.font = `${fontSize}px Arial`;
 
-    ctx.fillStyle = "#ffffff";
-    ctx.fillText(data, x, y);
-    ctx.stroke();
-    ctx.fillStyle = "#000000";
+  const textWidth = ctx.measureText(data).width;
+  const tagWidth = textWidth + 2 * padding;
+  const tagHeight = fontSize + 6;
+
+  roundRect(ctx, x - padding, y - fontSize + 2, tagWidth, tagHeight, 3);
+
+  ctx.fillStyle = "#ffffff";
+  ctx.fillText(data, x, y);
+  ctx.stroke();
+  ctx.fillStyle = "#000000";
 }
 
-
 export function generateImage(data: object) {
-    const topFiveProjects = ((data as any).user.repositories.nodes).sort(compareStars).slice(0, 5);
-    const contributions = (data as any).user.contributionsCollection.contributionCalendar.weeks
-    let contributionsProcessed: any = []
+  const languages = getLanguages(data);
+  const topFiveProjects = (data as any).user.repositories.nodes
+    .sort(compareStars)
+    .slice(0, 5);
+  const contributions = (data as any).user.contributionsCollection
+    .contributionCalendar.weeks;
+  let contributionsProcessed: any = [];
 
-    contributions.forEach((week: any) => {
-        week.contributionDays.forEach((day: any) => {
-            contributionsProcessed.push({date: day.date, contributionCount: day.contributionCount})
-        });
+  contributions.forEach((week: any) => {
+    week.contributionDays.forEach((day: any) => {
+      contributionsProcessed.push({
+        date: day.date,
+        contributionCount: day.contributionCount,
+      });
     });
+  });
 
-    let canvas = createCanvas(512, 200);
-    let ctx = canvas.getContext("2d");
+  let canvas = createCanvas(512, 200);
+  let ctx = canvas.getContext("2d");
 
-    generateGraph(ctx, contributionsProcessed, 512, 200, 8)
-    ctx.strokeStyle = "#ffffff"
-    generateGraph(ctx, contributionsProcessed, 512, 200, 0.8)
-    ctx.strokeStyle = "#000000";
-    ctx.lineWidth = 2;
+  generateGraph(ctx, contributionsProcessed, 512, 200, 8);
+  ctx.strokeStyle = "#ffffff";
+  generateGraph(ctx, contributionsProcessed, 512, 200, 0.8);
+  ctx.strokeStyle = "#000000";
+  ctx.lineWidth = 2;
 
-    topFiveProjects.forEach((project: any, index: number) => {
-        generateTag(ctx, 20, 20 + (25 * index), `${project.name} | ${project.stargazerCount} stars`);
+  topFiveProjects.forEach((project: any, index: number) => {
+    let projectName = project.name.substring(0, 18);
+    if (projectName != project.name) {
+      projectName += "...";
+    }
+    generateTag(
+      ctx,
+      20,
+      20 + 25 * index,
+      `${projectName} | ${project.stargazerCount} stars`
+    );
+  });
+
+  let languagePercentage: any = [];
+  Object.keys(languages.languages).forEach((key: string) => {
+    if(key == 'Shell') {
+        return;
+    }
+
+    languagePercentage.push({
+      name: key,
+      percentage: Math.round(
+        (languages.languages[key] / languages.total) * 100
+      ),
     });
+  });
 
-    return canvas.toBuffer("image/png");
+  const topTenLanguages = languagePercentage.sort(comparePercentage).slice(0, 5);
+  topTenLanguages.forEach((language: any, index: number) => {
+    generateTag(
+        ctx,
+        260,
+        20 + 25 * index,
+        `${language.name} | ${language.percentage} %`
+    );
+  });
+
+  return canvas.toBuffer("image/png");
 }
